043775e236d12de7dcd54272772da36fee23ebaa
unknown <reuelbodhak07@gmail.com>
2024-12-16 16:02:40 +0530

    implemented logic in navbar , added Icon1 svg

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <Pokeball className="opacity-15 fixed scale-[1.4] sm:scale-[2.3] sm:translate-y-5 sm:translate-x-4 md:scale-[2.5]  rotate-45" />
-      <main className="h-screen flex px-8 py-8 sm:px-12 sm:py-8 md:px-14 md:py-10 w-full bg-slant-200">
+      <main className="h-screen flex px-8 py-8 sm:px-12 sm:py-8 md:px-14 md:py-10 w-full bg-gray-100">
         <Navbar />
       </main>
     </Provider>
--- src/componets/Navbar/Navbar.tsx
+++ src/componets/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { UseSelector, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { change } from "../../features/state";
+import Icon1 from "../SVG/Icon1";
 
 const Navbar: React.FC = () => {
   const dispatch = useDispatch();
@@ -13,7 +14,45 @@ const Navbar: React.FC = () => {
 
   return (
     <>
-      <div className="bg-white rounded-xl shadow-lg z-10 w-full h-[65px] sm:h-[75px] md:h-[80px]"></div>
+      <div className="bg-white transition-all gap-5 md:gap-7 md:px-12 lg:px-14 lg:gap-10 duration-150 flex px-5 sm:px-9 rounded-xl shadow-lg z-10 w-full h-[65px] sm:h-[70px]">
+        <button
+          onClick={() => handleOptionChange("home")}
+          className={`${
+            currentOption === "home" ? "border-red-500 border-b-2" : ""
+          } flex hover:opacity-60 transition-opacity duration-150 flex-row sm:gap-2 px-2  items-center`}
+        >
+          {" "}
+          <Icon1
+            fillColor={`${currentOption === "home" ? "red" : "gray"}`}
+            className="h-7 w-12 sm:h-9"
+          />
+          <p
+            className={`${
+              currentOption === "home" ? "text-red-500" : "text-gray-500"
+            } font-bold text-md sm:text-lg `}
+          >
+            Pokedex
+          </p>
+        </button>
+        <button
+          onClick={() => handleOptionChange("about")}
+          className={`${
+            currentOption === "about" ? "border-b-2 border-red-500" : ""
+          } flex hover:opacity-60 transition-all duration-150 flex-row sm:gap-2 px-1  items-center`}
+        >
+          <Icon1
+            fillColor={`${currentOption === "about" ? "red" : "gray"}`}
+            className="h-7 w-12 sm:h-9"
+          />
+          <p
+            className={`${
+              currentOption === "about" ? "text-red-500" : "text-gray-500"
+            } font-bold text-md sm:text-lg `}
+          >
+            About
+          </p>
+        </button>
+      </div>
     </>
   );
 };
